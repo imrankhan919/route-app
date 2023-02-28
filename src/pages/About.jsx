2125ecc56af8b995f81f4d2319ff0c80d060f717
imrankhan919 <imrankhan919.ik@gmail.com>
2023-02-28 15:34:32 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='container p-5'>
+    <h1 className='text-center'>About</h1>
+</div>
+  )
+}
+
+export default About
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='container p-5'>
+    <h1 className='text-center'>Contact</h1>
+</div>
+  )
+}
+
+export default Contact
